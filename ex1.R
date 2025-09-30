b8b5d5679f1082eb89bdc3ae37efb15caa788d99
DAHAN Gabriel <id2963@clust-n2.domensai.ecole>
2025-09-30 12:43:04 +0200

    Ex.1 - 1 to 29

--- ex1.R
+++ ex1.R
@@ -51,7 +51,78 @@ tapply(titanic_place$Survie, titanic_place$Classe, sum)
 #--- 11. Déjà fait.
 
 #--- 12. 
+c1 <- read.csv2('./test_files/titanic_client1.csv', sep = ',')
+c2 <- read.csv2('./test_files/titanic_client2.csv', sep = ',')
+c3 <- read.csv2('./test_files/titanic_client3.csv', sep = ',')
+
+titanic_client <- rbind(c1, c2, c3)
+
+#--- 13.
+question13 <- read_excel("./test_files/titanic_place.xlsx") 
+question13$Prix <- as.factor(question13$Prix)
+
+#--- 14.
+id14_15_16 <- question13[question13$id %in% c("id14","id15","id16"),]
+id14_15_16$Prix <- as.numeric(id14_15_16$Prix) # Pas bien, car renvoie les modalités plutôt que les valeurs : fausse toutes les données.
+
+#--- 15.
+rm(id14_15_16)
+
+#--- 16.
+question13$Prix <- as.numeric(as.character(question13$Prix))
+
+#--- 17.
+dup <- duplicated(titanic_client)
+any(dup) #... oui
+
+titanic_client <- titanic_client[-dup,]
+titanic_client$Age <- as.numeric(titanic_client$Age)
+
+#--- 18.
+titanic_passager <- merge(titanic_client, titanic_place, by.x = 'id', by.y = 'id')
+
+#--- 19.
+titanic_svm <- titanic_passager[-which(is.na(titanic_passager$Age)),]
+
+#--- 20.
+titanic_svm$Sexe <- factor(
+  ifelse(titanic_svm$Civilite %in% c('ColM', 'DrM', 'Master', 'Mr', 'Major', 'Rev'), 'Homme', 
+      ifelse(titanic_svm$Civilite %in% c('DrF', 'Miss', 'Mrs', 'Ms', 'Mme', 'Mlle', 'ColF', 'Dona'), 'Femme', 'Inconnu')
+  )
+)
+
+#--- 21.
+mean(titanic_svm$Age)
+mean(titanic_svm[titanic_svm$Sexe == 'Femme',]$Age)
+mean(titanic_svm[titanic_svm$Sexe == 'Homme',]$Age)
+
+#--- 22.
+boxplot(titanic_svm$Age ~ titanic_svm$Classe)
+
+#--- 23.
+length(which(titanic_svm$Survie == 1 & titanic_svm$Sexe == 'Homme' & titanic_svm$Classe == '1'))
+length(which(titanic_svm$Survie == 1 & titanic_svm$Sexe == 'Homme' & titanic_svm$Classe == '2'))
+length(which(titanic_svm$Survie == 1 & titanic_svm$Sexe == 'Homme' & titanic_svm$Classe == '3'))
+
+#--- 24.
+titanic_svm$Sexe_modif <- factor(ifelse(titanic_svm$Age > 12, ifelse(titanic_svm$Sexe == 'Homme', 'Homme', 'Femme'), 'Enfant'))
+
+#--- 25.
+t <- table(titanic_svm$Sexe_modif)
+t
+p1 <- prop.table(t)
+
+#--- 26.
+t <- table(titanic_svm[titanic_svm$Survie == '1',]$Sexe_modif)
+t
+p2 <- prop.table(t)
+
+#--- 27.
 
 
+#--- 28.
+table(titanic_svm$PortEmbarq, titanic_svm$Classe)
 
+#--- 29.
+table(titanic_svm$PortEmbarq, titanic_svm$Classe, titanic_svm$Survie)
 
